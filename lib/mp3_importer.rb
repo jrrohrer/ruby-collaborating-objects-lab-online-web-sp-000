b6a11551842f29bb37f10e0b554e867aa663e183
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-14 23:47:20 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,12 @@
 class MP3Importer
+  attr_accessor :path
+  
   def initialize(file_path)
     @path = file_path
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| == "." || file == ".."}
   end
 end
