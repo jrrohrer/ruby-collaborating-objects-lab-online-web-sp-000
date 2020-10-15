7eed8276dc6af43c6382d3df5c85eda076bcf023
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-15 00:53:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,8 +22,8 @@ class Song
   end
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    Artist.add_song(song.name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(song.name)
   end
 
 end
