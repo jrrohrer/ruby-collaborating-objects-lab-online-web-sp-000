49b3c1e4d05496c7a971862dcb6ec0e29ef062df
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-15 00:29:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,9 @@ class Song
 
   def self.new_by_filename(filename)
     splitname = filename.split(" - ")
+    song = self.new(splitname[1])
+    artist = Artist.find_or_create_by_name(splitname[0])
+    song.artist = artist
   end
 
   def artist_name=
